99799ecd125be9d58b0fdee19bd2c51b810056df
Jonathan Wilde <hello@jwilde.me>
2014-11-15 00:53:12 -0500

    Adding StringCursor, which we'll be using to implement FullHashRequestType's parser.

---
+++ src/utils/StringCursor.js
@@ -0,0 +1,35 @@
+class StringCursor {
+  constructor(str) {
+    this._str = str;
+    this._offset = 0;
+  }
+
+  remaining() {
+    return this._str.length - this._offset;
+  }
+
+  chompUntil(delimiter) {
+    var offset = this._offset;
+    while (this._str.charAt(offset) != delimiter && offset < this._str.length) {
+      offset++;
+    }
+
+    var slice = this._str.slice(this._offset, offset);
+    this._offset = offset + 1;
+    return slice;
+  }
+
+  chompLength(length) {
+    var slice = this._str.slice(this._offset, this._offset + length);
+    this._offset = Math.min(this._offset + length, this._str.length);
+    return slice;
+  }
+
+  chompRemaining() {
+    var slice = this._str.slice(this._offset);
+    this._offset = this._str.length;
+    return slice;
+  }
+}
+
+module.exports = StringCursor;
---
+++ src/utils/__tests__/StringCursor-test.js
@@ -0,0 +1,45 @@
+var StringCursor = require('../StringCursor');
+
+var expect = require('expect');
+
+describe('StringCursor', function () {
+  describe('chompUntil', function() {
+    it('should return empty on empty', function() {
+      expect(new StringCursor('').chompUntil(':')).toBe('');
+    });
+
+    it('should return sections between delim', function() {
+      var cursor = new StringCursor('dsfa:sadfggdfsg:');
+      expect(cursor.chompUntil(':')).toBe('dsfa');
+      expect(cursor.chompUntil(':')).toBe('sadfggdfsg');
+      expect(cursor.chompUntil(':')).toBe('');
+    })
+  });
+
+  describe('chompLength', function() {
+    it('should return empty on empty', function() {
+      expect(new StringCursor('').chompLength(5)).toBe('');
+    });
+
+    it('should return sections with no gap', function() {
+      var cursor = new StringCursor('dsfa:sadfggdfsg:');
+      expect(cursor.chompLength(5)).toBe('dsfa:');
+      expect(cursor.chompLength(5)).toBe('sadfg');
+      expect(cursor.chompLength(5)).toBe('gdfsg');
+      expect(cursor.chompLength(5)).toBe(':');
+    });
+  });
+
+  describe('chompRemaining', function() {
+    it('should return empty on empty', function() {
+      expect(new StringCursor('').chompRemaining()).toBe('');
+    });
+
+    it('should return remainder chomped', function() {
+      var cursor = new StringCursor('dsfa:sadfggdfsg:');
+      expect(cursor.chompLength(5)).toBe('dsfa:');
+      expect(cursor.chompLength(5)).toBe('sadfg');
+      expect(cursor.chompRemaining()).toBe('gdfsg:');
+    });
+  });
+});
