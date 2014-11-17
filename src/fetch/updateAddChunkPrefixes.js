621e77c66f56c71089f40c89ed02733b2ade6662
Jonathan Wilde <hello@jwilde.me>
2014-11-17 03:04:33 -0500

    Following short form docs rule.

--- src/fetch/updateAddChunkPrefixes.js
+++ src/fetch/updateAddChunkPrefixes.js
@@ -3,12 +3,6 @@ var Promise = require('bluebird');
 /**
  * Adds all of the prefixes that haven't been markd with a pending sub.
  * Drop pending sub chunks for things that we're skipping over.
- * 
- * @param  {Cache}  cache    Instance of the cache to operate against.
- * @param  {String} listName List name in the cache to operate against.
- * @param  {Number} chunkID  Chunk number that prefixes are associated with.
- * @param  {Array}  prefixes Prefixes from an add chunk of number chunkID.
- * @return {Promise}         Resolves when the prefixes have been added.
  */
 function updateAddChunkPrefixes(cache, listName, chunkID, prefixes) {
   return Promise.filter(prefixes, function(prefix) {
--- src/fetch/updateSubChunkPrefixes.js
+++ src/fetch/updateSubChunkPrefixes.js
@@ -4,13 +4,6 @@ var ChunkTypes = require('./ChunkTypes');
 /**
  * For all of our <prefix, addNumber> combinations: if we have a stored add
  * chunk with number addNumber, drop the prefix; else, put in a pending sub.
- * 
- * @param  {Cache}    cache      [description]
- * @param  {String}   listName   [description]
- * @param  {Number}   chunkID    [description]
- * @param  {Array}    prefixes   [description]
- * @param  {Array}    addNumbers [description]
- * @return {Promise}              [description]
  */
 function updateSubChunkPrefixes(cache, listName, chunkID, prefixes, addIDs) {
   return Promise.filter(prefixes, function(prefix, idx) {
